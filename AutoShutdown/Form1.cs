da8fcf03a1008abe48aa26c8e834c8c39e1038e9
Frank Refol <t3knoid@live.com>
2018-11-17 19:56:30 -0500

    Add menu and minimiize to tray code

--- AutoShutdown/Form1.cs
+++ AutoShutdown/Form1.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Drawing;
+﻿using Microsoft.Win32;
+using System;
 using System.Reflection;
-using System.Threading;
 using System.Windows.Forms;
-using Microsoft.Win32;
 
 namespace AutoShutdown
 {
@@ -17,12 +13,20 @@ namespace AutoShutdown
         private MenuStrip menuStrip1;
         private PictureBox pictureBox1;
         private Logger logger;
+        private ToolStripMenuItem fileStripMenuItem;
+        private ToolStripMenuItem minimizeToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private NotifyIcon notifyIcon1;
+        private System.ComponentModel.IContainer components;
         private bool Offline;
+        private String status = "Offline";
 
         public Form1()
         {
             logger = new Logger(String.Format("{0}.log", System.Reflection.Assembly.GetEntryAssembly().Location));
             logger.Info("Starting AutoShutdown.","");
+            SetPowerStatus();
             InitializeComponent();
             SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged); //https://docs.microsoft.com/en-us/dotnet/api/microsoft.win32.systemevents.powermodechanged
             this.ResumeLayout(false);
@@ -31,6 +35,7 @@ namespace AutoShutdown
 
         private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
         {
+            SetPowerStatus();
             ShowPowerStatus();
             if (Offline)
             {
@@ -39,12 +44,10 @@ namespace AutoShutdown
             }
         }
         /// <summary>
-        /// Determines and displays the value of the PowerLineStatus property.
-        /// https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.powerstatus?view=netframework-4.7.2
+        /// Sets power status
         /// </summary>
-        private void ShowPowerStatus()
+        private void SetPowerStatus()
         {
-            // Display the value of the selected property of the PowerStatus type.
             // https://docs.microsoft.com/en-us/dotnet/api/system.windows.powerlinestatus?view=netframework-4.7.2
             Type t = typeof(System.Windows.Forms.PowerStatus);
             PropertyInfo[] pi = t.GetProperties();
@@ -55,21 +58,39 @@ namespace AutoShutdown
                     prop = pi[i];
                     break;
                 }
-            
+
             object propval = prop.GetValue(SystemInformation.PowerStatus, null);
             logger.Info(String.Format("PowerLineStatus is set to {0}", propval.ToString()));
             if (propval.ToString() == "Offline")
-            { 
+            {
+                status = "Offline";
                 Offline = true;
+            }
+            else
+            {
+                status = "Online";
+                Offline = false;
+            }
+        }
+        /// <summary>
+        /// Displays power indicator
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.powerstatus?view=netframework-4.7.2
+        /// </summary>
+        private void ShowPowerStatus()
+        {
+            if (Offline)
+            {
                 // Show shutdown image
                 pictureBox1.Image = global::AutoShutdown.Properties.Resources.Power___Shut_Down_128x128;
             }
             else
             {
-                Offline = false;
                 // Show power on image
                 pictureBox1.Image = global::AutoShutdown.Properties.Resources.power_128;
             }
+            this.notifyIcon1.BalloonTipText = status;
+            this.notifyIcon1.Text = status;
+            notifyIcon1.ShowBalloonTip(500);
         }
         [STAThread]
         static void Main()
@@ -79,28 +100,72 @@ namespace AutoShutdown
 
         private void InitializeComponent()
         {
-            this.Icon = global::AutoShutdown.Properties.Resources.Icon1;
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minimizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(167, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // pictureBox1
             // 
-            //this.pictureBox1.Image = global::AutoShutdown.Properties.Resources.Power___Shut_Down_128x128;
             this.pictureBox1.Location = new System.Drawing.Point(21, 44);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(128, 129);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(167, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileStripMenuItem
+            // 
+            this.fileStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minimizeToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileStripMenuItem.Name = "fileStripMenuItem";
+            this.fileStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileStripMenuItem.Text = "File";
+            // 
+            // minimizeToolStripMenuItem
+            // 
+            this.minimizeToolStripMenuItem.Name = "minimizeToolStripMenuItem";
+            this.minimizeToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.minimizeToolStripMenuItem.Text = "Minimize";
+            this.minimizeToolStripMenuItem.Click += new System.EventHandler(this.minimizeToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = global::AutoShutdown.Properties.Resources.Icon1;
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
             // 
             // Form1
             // 
@@ -108,12 +173,40 @@ namespace AutoShutdown
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = global::AutoShutdown.Properties.Resources.Icon1;
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
+        }
+
+        private void minimizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(500);
+            this.Hide();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AutoShutdown by Frank Refol\n© 2018 Frank Refol All Rights Reserved","About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            logger.Info("Exiting application");
+            this.notifyIcon1.Visible = false;
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
         }
     }
 }
